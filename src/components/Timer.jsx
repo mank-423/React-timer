20703774acb79294f09bfacb1be727d8b7a83ced
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-11-09 17:43:10 +0530

    Integrate API for quotes

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -8,8 +8,17 @@ const Screen2 = () => {
     const [mn, setMn] = useState(min);
     const totalTime = hr * 3600 + mn * 60;
     const [remainingTime, setRemainingTime] = useState(totalTime);
+    const [quote, setQuote] = useState('');
+    const [author, setAuthor] = useState('');
 
     let navigate = useNavigate();
+
+    const apiCall = async() => {
+        const response = await fetch("https://api.quotable.io/quotes/random?tags='success' & maxLength=100");
+        const data = await response.json();
+        setQuote(data[0].content);
+        setAuthor(data[0].author);
+    }
 
     const updateTime = () => {
         if (remainingTime > 0) {
@@ -33,6 +42,10 @@ const Screen2 = () => {
         };
     }, [remainingTime]);
 
+    useEffect(()=>{
+        apiCall();
+    },[])
+
     // Format and display the remaining time
     const hours = Math.floor(remainingTime / 3600);
     const minutes = Math.floor((remainingTime % 3600) / 60);
@@ -47,6 +60,11 @@ const Screen2 = () => {
                 <p className="flex justify-center place-items-center text-7xl md:text-8xl lg:text-9xl timer-font">
                     {formattedTime}
                 </p>
+            </div>
+
+            <div className='timer-font px-20 landscape:mb-10 pb-10'>
+                <p className='flex items-center justify-center sm:text-sm lg:text-lg'>{quote}</p>
+                <p className='flex items-center justify-center sm:text-sm lg:text-lg'>-{author}</p>
             </div>
         </div>
     );
